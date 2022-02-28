a9fb2cb2c49402e16f90c3ae8f4e6fd7e9a21bd4
shohamyech <shohamyech@gmail.com>
2022-02-28 17:10:28 +0200

    resolved read-line repl issue, moved to main

--- src/clo_proj/core.clj
+++ src/clo_proj/core.clj
@@ -3,18 +3,6 @@
   (:require [clojure.java.io :as io])
   (:gen-class))
 
-(println "Enter path")
-(def path (read-line))
-
-;checking folder exists
-(when-not (.exists (java.io.File. path))
-  (println "Invalid path")
-  (System/exit 0))
-
-;constructing the output file name - the same as the folder
-(def output-file 
-  (str path "\\" (re-find #"\w*$" path) ".asm"))
-
 (def total-buy  0)
 (def total-sell 0)
 
@@ -35,8 +23,7 @@
     (alter-var-root #'total-sell (constantly (+ total-sell cost)))
 
     (str "$$$ SELL " (get s 1) " $$$\n"
-         (format "%,.2f" cost) "\n"))
-)
+         (format "%,.2f" cost) "\n")))
 
 ;recieves ["buy\sell" "bread" "3" "4.5"] (example)
 ;if first value is buy calls HandleBuy else calls HandleSell sell
@@ -55,29 +42,43 @@
               (map buy-or-sell))))
 
 ;gets vm files names from folder
-(def files-names 
-(->> (io/file path)
-     (file-seq)
-     (filter #(.isFile %))
-     (map str)
-     (map #(re-find #"\w*.vm$" %))
-     (remove nil?)
-     (map #(str/replace % #".vm$" ""))))
+(defn files-names [path]
+  (->> (io/file path)
+       (file-seq)
+       (filter #(.isFile %))
+       (map str)
+       (map #(re-find #"\w*.vm$" %))
+       (remove nil?)
+       (map #(str/replace % #".vm$" ""))))
 
-(->> (io/file path)                      ;get folder file obj
-     (file-seq)                          ;get all files from folder
-     (filter #(.isFile %))               ;filter files
-     (map str)                           ;map to files path as string
-     (filter #(re-find #".vm$" %))       ;filter vm files
-     (map slurp)                         ;map to the files content
-     (map vector files-names)            ;add file name to each file content
-     (map HandleFile)                    ;convert to HandleFile result
-     (str/join)                          ;join all files
-     (spit output-file))                 ;write to output file
+(defn -main []
 
-;writing total buy and sell to output file
-(spit output-file
-      (str "\nTOTAL BUY: " (format "%,.2f" total-buy)
-           "\nTOTAL SELL: " (format "%,.2f" total-sell)) :append true)
+  (println "Enter path")
 
+  (let [path (read-line)
+        output-file ;constructing the output file name - the same as the folder
+        (str path "\\" (re-find #"\w*$" path) ".asm")]
+
+    ;checking folder exists
+    (when-not (.exists (java.io.File. path))
+      (println "Invalid path")
+      (System/exit 0))
+
+
+    (->> (io/file path)                      ;get folder file obj
+         (file-seq)                          ;get all files from folder
+         (filter #(.isFile %))               ;filter files
+         (map str)                           ;map to files path as string
+         (filter #(re-find #".vm$" %))       ;filter vm files
+         (map slurp)                         ;map to the files content
+         (map vector (files-names path))     ;add file name to each file content
+         (map HandleFile)                    ;convert to HandleFile result
+         (str/join)                          ;join all files
+         (spit output-file))                 ;write to output file
+
+    ;writing total buy and sell to output file
+    (spit output-file
+          (str "\nTOTAL BUY: " (format "%,.2f" total-buy)
+               "\nTOTAL SELL: " (format "%,.2f" total-sell)) :append true))
+  (str "Finished!"))
 
